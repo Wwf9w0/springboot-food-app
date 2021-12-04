31e7b42f7bd7c49038b1b0b1383baf4942144c5a
emrekaradag <aliemrekaradag1@gmail.com>
2021-12-04 20:41:19 +0300

    OrderController created

--- src/main/java/com/food/app/controller/OrderController.java
+++ src/main/java/com/food/app/controller/OrderController.java
@@ -1,4 +1,40 @@
 package com.food.app.controller;
 
+import com.food.app.Service.OrderService;
+import com.food.app.persistence.jpa.entity.order.entity.Order;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/v1/order")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/create/order")
+    @ApiOperation("Create Order")
+    public ResponseEntity<Order> save(@RequestBody Order order){
+        Order o = orderService.save(order);
+        return ResponseEntity.ok(o);
+    }
+
+    @GetMapping("/orders")
+    @ApiOperation("List of Orders")
+    public ResponseEntity<List<Order>> getOrders(){
+        List<Order> orderList = orderService.getOrders();
+        return ResponseEntity.ok(orderList);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ApiOperation("Delete Order")
+    public ResponseEntity<Order> deleteOrder(@PathVariable("id") Long id){
+        orderService.deleteOrderById(id);
+        return ResponseEntity.ok().build();
+    }
+
 }

0ce6a98048383e6e4f9cfea2d6485b1c42c06146
wwf9w0 <aliemrekaradag1@gmail.com>
2022-03-05 01:47:59 +0300

    added OrderConverter

--- src/main/java/com/food/app/persistence/jpa/converter/OrderConverter.java
+++ src/main/java/com/food/app/persistence/jpa/converter/OrderConverter.java
@@ -1,4 +1,17 @@
 package com.food.app.persistence.jpa.converter;
 
+import com.food.app.persistence.jpa.dto.OrderDto;
+import com.food.app.persistence.jpa.entity.order.entity.Order;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrderConverter {
+
+    public OrderDto toOrderDto (Order order){
+        return OrderDto.builder()
+                .id(order.getId())
+                .note(order.getNote())
+                .totalPrice(order.getTotalPrice())
+                .build();
+    }
 }
